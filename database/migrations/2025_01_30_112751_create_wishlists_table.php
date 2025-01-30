c860655b0382bcadebf673a2d9077b9af799ddf6
Nathanael Ferry Pratama <ntnferry1505@gmail.com>
2025-01-30 21:26:55 +0700

    feat: migrations

--- database/migrations/2025_01_30_112751_create_wishlists_table.php
+++ database/migrations/2025_01_30_112751_create_wishlists_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('product_id')->constrained('products');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('wishlists');
     }
 };
